ec07f911b6ecf0d086322a7025fc07932307b428
Henrique Noronha Facioli <ra157986@mamonas.lab.ic.unicamp.br>
2017-04-20 11:13:30 -0300

    Client done

--- proj1/client.c
+++ proj1/client.c
@@ -4,8 +4,6 @@
 #include <unistd.h>
 #include <limits.h>
 #include <netdb.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 
 #define SERVER_PORT 12345
 #define MAX_LINE 256
@@ -17,25 +15,25 @@
 int main(int argc, char * argv[])
 {
     struct hostent *host_address;
-    struct sockaddr_in* socket_address;
+    struct sockaddr_in socket_address;
     char *host;
-    char buf[MAX_LINE];
+    char buf[MAX_LINE], msg[MAX_LINE];
     int s, len, port, c;
 
     /* Checking arguments */
-    if (argc < 1){
+    if (argc < 2){
         printf("Missing Host Address");
         return -1;
-    } else if (argc < 2){
+    } else if (argc < 3){
         port = SERVER_PORT;
     } else {
         port = atoi(argv[2]);
     }
 
-    /* Trabslate the host name to IP*/
+    /* Translate the host name to IP*/
     host_address = gethostbyname(argv[1]);
     if (host_address == NULL){
-        printf("Error on gethostbyname");
+        printf("Error on gethostbyname\n");
         return -3;
     }
 
@@ -47,22 +45,38 @@ int main(int argc, char * argv[])
      */
     s = socket(AF_INET, SOCK_STREAM, 0);
     if ( s < 0 ) {
-        printf("Problem occurred when creating a socket");
+        printf("Problem occurred when creating a socket\n");
         return -2;
     }
 
     /* Allocate the socket  and fills with the necessary info */
-    /*bzero((char *)&socket_address, sizeof(socket_address)); */
-    socket_address = (struct sockaddr_in*) calloc(1, sizeof(struct sockaddr_in*));
-    socket_address->sin_family = AF_INET;
-    socket_address->sin_port = htons(port);
-    bcopy(&socket_address->sin_addr, host_address->h_addr_list[0], 4);
+    bzero((char *)&socket_address, sizeof(socket_address));
+    //socket_address = (struct sockaddr_in*) calloc(1, sizeof(struct sockaddr_in*));
+    socket_address.sin_family = AF_INET;
+    socket_address.sin_port = htons(port);
+    socket_address.sin_addr = *(struct in_addr*) host_address->h_addr_list[0];
 
     /* criação de socket ativo*/
     c = connect(s,(struct sockaddr*) &socket_address, sizeof(struct sockaddr_in));
     if (c < 0) {
-        printf("Problem occurred when connecting");
+        printf("Problem occurred when connecting\n");
+        return -4;
     }
-    /* estabelecimento da conexão */
+   
+    printf("Escreva a mensagem a ser enviada\n");
+    bzero(buf,MAX_LINE);
+    bzero(msg, MAX_LINE);
+    fgets(buf,MAX_LINE,stdin);
+    if(send(s, (const void*) buf, MAX_LINE, 0) < 0 ){
+        printf("Problem sending message\n");
+        return -5;
+    }
+
+    if (recv(s, (void *) msg, MAX_LINE, 0) < 0 ){
+        printf("Could not receive any response\n");
+        return -6;
+    }
+    printf("%s", msg);
     /* ler e enviar linhas de texto, receber eco */
+
 }
