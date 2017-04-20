dc1cdad69f0ce2351acad481ca239169c273595c
Henrique Noronha Facioli <ra157986@mamonas.lab.ic.unicamp.br>
2017-04-20 11:28:35 -0300

    Tips for server

--- proj1/server.c
+++ proj1/server.c
@@ -8,24 +8,30 @@
 #define MAX_PENDING 5
 #define MAX_LINE 256
 
-int main()
-{
+//sin_addr-s_addrhtonl(INADDR_ANY)
+
+int main() {
 	struct sockaddr_in socket_address;
     char buf[MAX_LINE];
     unsigned int len;
     int s, new_s;
-
+    
+    /* Checking arguments */
+    if (argc < 2){
+        printf("Missing Host Address");
+        return -1;
+    } else if (argc < 3){
+        port = SERVER_PORT;
+    } else {
+        port = atoi(argv[2]);
+    }
+    
     /* criação da estrutura de dados de endereço */
     bzero((char *)&socket_address, sizeof(socket_address));
-	(...)
 	/* criação de socket passivo */
-	(...)
 	/* Associar socket ao descritor */
-	(...)
-	/* Criar escuta do socket para aceitar conexões */
-	(...)
-        /* aguardar/aceita conexão, receber e imprimir texto na tela, enviar eco */
-	(...)
+    /* Criar escuta do socket para aceitar conexões */
+	/* aguardar/aceita conexão, receber e imprimir texto na tela, enviar eco */
 }
 
 
