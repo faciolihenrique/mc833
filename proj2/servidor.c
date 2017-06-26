98c0ab0361b25f56bfa656c2b9a805a07b075747
henriquefacioli <henrique.facioli@gmail.com>
2017-06-26 18:18:47 -0300

    nothing

--- proj2/servidor.c
+++ proj2/servidor.c
@@ -6,8 +6,16 @@
  * Thiago Silva de Farias
  */
 
+#include "main.h"
+#include "servidor.h"
+
 void create_router() {
-    create_media_server()
+    
+    create_media_server();
     create_security_server();
     create_confort_server();
+}
+
+void create_media_server() {
+
 }
