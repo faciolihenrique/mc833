f4609cd4e175663ea98d376188ca28a17be7c131
henriquefacioli <henrique.facioli@gmail.com>
2017-06-28 19:51:16 -0300

    Organization of definitions

--- proj2/definitions.h
+++ proj2/definitions.h
@@ -7,22 +7,33 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
-#define SIMULATE1                 // Change to which simulation
+////////////////////////////////////////////////////////////////////////////////
+// Definitions from simulation
 
-#define CON_TCP                     // Set to TCP or UDP
-#define ENT_TCP                     // Set to TCP or UDP
-#define SEC_TCP                     // Set to TCP or UDP
+#define SIMULATE1                   // Change to which simulation
+
 #define SIZE_MAP 18                 // Radius of the comunication with the router
 #define MAX_SPEED 3                 // Streets max speed
 #define N_CONNECTIONS 20            // How many cars can talk to the router
 #define PACKAGE_INTERVAL 25         // Time to send the packages (in ms)
 #define SPEED_INTERVAL 10           // Block_walking = Speed/SPEED_INTERVAL(in ms)
 
+////////////////////////////////////////////////////////////////////////////////
+// Definitions from server
 
+#define CON_TCP                     // Set to TCP or UDP
+#define ENT_TCP                     // Set to TCP or UDP
+#define SEC_TCP                     // Set to TCP or UDP
+#define DELAY_CON 100
+#define DELAY_ENT 100
+#define DELAY_SEC 100
 #define SEC_ADDRESS "localhost"
 #define CON_ADDRESS "localhost"
 #define ENT_ADDRESS "localhost"
 
-#define NCURSES_SIMULATE            // Enable this if want to see cars walking
+////////////////////////////////////////////////////////////////////////////////
+// Define if should print the info from the packages or should demonstrate the
+// simulation on NCURSES
+#define NCURSES_SIMULATE              // Enable this if want to see cars walking
 
 #endif
