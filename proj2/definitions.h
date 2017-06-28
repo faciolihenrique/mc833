b4421832fb467189cd64d350148a5f334d73e404
henriquefacioli <henrique.facioli@gmail.com>
2017-06-28 01:13:58 -0300

    Just a comment

--- proj2/definitions.h
+++ proj2/definitions.h
@@ -11,7 +11,7 @@
 
 #define TCP                         // Use TCP or UDP
 #define SIZE_MAP 18                 // Radius of the comunication with the router
-#define MAX_SPEED 3                 // Streets max speed
+#define MAX_SPEED 1                 // Streets max speed
 #define N_CONNECTIONS 20            // How many cars can talk to the router
 #define PACKAGE_INTERVAL 25         // Time to send the packages (in ms)
 #define SPEED_INTERVAL 10           // Block_walking = Speed/SPEED_INTERVAL(in ms)
